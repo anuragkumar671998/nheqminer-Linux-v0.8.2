dbb6ce8085f41559f5201fd14f7c064a1a6da5cc
anuragkumar671998 <40496319+anuragkumar671998@users.noreply.github.com>
2021-12-11 18:57:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-nheqminer -v -l ap.luckpool.net:3956 -u RQSZxYyFk7qt4N7XyS2MvEtsNkjshw17LF.Linux -p x -t 4
+nheqminer -v -l ap.luckpool.net:3956 -u RQSZxYyFk7qt4N7XyS2MvEtsNkjshw17LF.LinuxA -p x -t 4
